1501988d528ce74812479c8fb727b6dea0de2668
Miguel Lopes <82033110+El1ters@users.noreply.github.com>
2021-05-17 13:40:38 +0100

    Add files via upload

--- CompareName.c
+++ CompareName.c
@@ -1,2 +1,18 @@
-#include "main.c"
+#include "main.h"
 
+int CompareName(Country *listhead,char *string){
+	Country *AuxH;
+
+	if(listhead == NULL){
+		return 0;
+	}
+	else{
+		for(AuxH = listhead; AuxH != NULL;AuxH = AuxH->next_country){
+			if(strcmp(AuxH->name,string) == 0){
+				return 1;
+			}
+		}
+	}
+	return 0;
+
+}
