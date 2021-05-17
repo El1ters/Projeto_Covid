54986a6846d339618d781f70eac4ffaf901960d1
DiogoGrilo240202 <82812987+DiogoGrilo240202@users.noreply.github.com>
2021-05-17 23:10:12 +0100

    Add files via upload

--- StoreContentCountry.c
+++ StoreContentCountry.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-
+/*Função que permite a criação de um nó da lista principal -> nó este que contém os dados fixos de cada país*/
 void StoreContentCountry(char content[9][70],Country *country){
 		strcpy(country->name,content[0]);
 		strcpy(country->country_code,content[1]);
-		strcpy(country->continent,content[2]);			
+		strcpy(country->continent,content[2]);
 		country->population = atoi(content[3]);
 }
