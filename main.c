cc1e78a39c04c243ae676ba44183e880887793ef
DiogoGrilo240202 <82812987+DiogoGrilo240202@users.noreply.github.com>
2021-05-20 12:22:12 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,40 +1,11 @@
 #include "main.h"
 
 
+
 int main(int argc,char *argv[]){
-	char L[10], S[10],D[10],P[10],i[10],o[10];
-	CommandLine(argc,argv,L,S,D,P,i,o);
-	/*Inicialização de um ponteiro que aponta para o tipo de estrutura Country -> este
-    ponteiro corresponde à cabeça da lista principal (a que contém os nomes dos diferentes países*/
-	Country *ListHead = NULL;
-	/*Criação de um ponteiro que aponta para o tipo de estrutura Country - vai servir para
-    criar novos nós da lista principal que, por sua vez, contém os diferentes países*/
-	Country *newcountry;
 
-	FILE *fp;
-	/*Abertura para leitura do ficheiro criado que irá ter o título indicado*/
-	if((fp = fopen("covid19_w_tf01.csv","r")) == NULL){
-		printf("could not open the file\n");
-		exit(EXIT_FAILURE);
-	}
+    CommandLine(argc, argv);
 
-	char line[128]; 
-
-	/*Usa-se um fgets para ignorar o cabeçalho*/
-	fgets(line,128,fp);
-	/*Este ciclo while que se segue vai percorrer linha a linha todo o ficheiro fornecido pelo professor e irá criar um
-	novo nó da lista principal (a que contém os nomes dos países) sempre que encontarar uma linha com um nome de um
-	país para o qual ainda não existe nó -> deste modo temos assim a nossa Lista principal criada*/
-	while(fgets(line,128,fp) != NULL){
-		if((newcountry = ReadFile(&ListHead,line)) != NULL){
-			ListHead = CriaListaPorBaixo(ListHead,newcountry);
-		}
-		
-	}
-	printLista(ListHead);
-	free_list(ListHead);
-
-	fclose(fp);
 	return 0;
 }
 
@@ -42,7 +13,7 @@ int main(int argc,char *argv[]){
 simplesmente ligada*/
 Country *CriaListaPorBaixo(Country *listhead,Country *country_inserted){
 	Country *Aux;
-	
+
 	if(listhead == NULL){
 		listhead = country_inserted;
 	}
@@ -63,7 +34,7 @@ void printLista(Country *listhead){
 
 	/*Aux no fim de cado ciclo aponta para a lista seguinte*/
 	for(Aux = listhead; Aux != NULL; Aux = Aux->next_country){
-		printf("===============País=================\n");
+		printf("===============Lista=================\n");
 		printf("Name: %s\n",Aux->name);
 		printf("Code: %s\n",Aux->country_code);
 		printf("Continent: %s\n",Aux->continent);
@@ -72,10 +43,10 @@ void printLista(Country *listhead){
 			printf("Ano: %d\n",AuxY->year);
 			for(AuxW = AuxY->next_week; AuxW != NULL; AuxW = AuxW->next_week){
 				printf("\tSemana: %d\n",AuxW->week);
-				//printf("\t Casos:\n");
-				//printf("\t  weekly_count:%d\n",AuxW->weekly_count_cases);
-				//printf("\t  rate_14_day:%.3f\n",AuxW->rate_14_day_cases);
-				//printf("\t  comulative_count:%d\n",AuxW->comulative_count_cases);
+				printf("\t Casos:\n");
+				printf("\t  weekly_count:%d\n",AuxW->weekly_count_cases);
+				printf("\t  rate_14_day:%.3f\n",AuxW->rate_14_day_cases);
+				printf("\t  comulative_count:%d\n",AuxW->comulative_count_cases);
 				printf("\t Mortes:\n");
 				printf("\t  weekly_count:%d\n",AuxW->weekly_count_deaths);
 				printf("\t  rate_14_day:%.3f\n",AuxW->rate_14_day_deaths);
@@ -85,7 +56,7 @@ void printLista(Country *listhead){
 
 		}
 	}
-	
+
 }
 
 void free_list(Country *listhead){
@@ -95,7 +66,7 @@ void free_list(Country *listhead){
 
 	for(Aux = listhead;Aux != NULL;Aux = listhead){
 		listhead = Aux->next_country;
-		for(AuxY = Aux->next_year; AuxY != NULL;AuxY = Aux->next_year){	
+		for(AuxY = Aux->next_year; AuxY != NULL;AuxY = Aux->next_year){
 			for(AuxW = AuxY->next_week;AuxW != NULL;AuxW = AuxY->next_week){
 				AuxY->next_week = AuxW->next_week;
 				free(AuxW);
