3c267d50d718f30b5083115ec8c881d8aa9ae5b9
DiogoGrilo240202 <82812987+DiogoGrilo240202@users.noreply.github.com>
2021-05-20 12:22:33 +0100

    Add files via upload

--- CommandLine.c
+++ CommandLine.c
@@ -1,59 +1,103 @@
 #include "main.h"
 
-void CommandLine(int argc, char *argv[], char *L, char *S, char *D, char *P, char *i, char *o){
+void CommandLine(int argc, char *argv[])
+{
 
     int opt=0;
-	char L_aux[10], S_aux[10], D_aux[10], P_aux[10], i_aux[10], o_aux[10];
+    char L[10], S[10], D[10], P[10], i[10], o[10];
 
-	//optind - index do próximo elemento a ser processado no argv, inicializado a 1
-	//optopt - guarda o caracter da opção, quando a opcao nao é reconhecida
-	//opterr - quando colocado a 0 evita a mensagem de erro
-	//optarg - apontador para texto do mesmo argumento
-	//optstring - string que contém a descrição das opções
+    while((opt = getopt(argc, argv,"hL:S:D:P:i:o: ")) != -1)
+    {
 
-	while((opt = getopt(argc, argv,"hL:S:D:P:i:o: ")) != -1){
-	// -1 quando não há mais opções para processar
-	// optstring : para indicar que a opção requer um parametro
-	// optstring :: para indicar que o parametro da opção é opcional, o parametro deve estar junto à opção sem espaço
-	// optstring - no começo quando se pretende processar os argumentos sem opção no loop while
-	  switch (opt){
+        switch (opt)
+        {
 
         case 'h':
             CommandLineHelp();
-        break;
+            break;
 
         case 'L':
             strcpy(L,optarg);
-        break;
+            /*fazer if's e string compares para ver se os dados inseridos pelo utilizador estão dentro das restrições*/
+            break;
 
         case 'S':
-            strcpy(S_aux,optarg);
-        break;
+            strcpy(S,optarg);
+            if (strcmp(S,"alfa")==0)
+            {
+
+            }
+            else if(strcmp(S,"pop")==0)
+
+                break;
 
         case 'D':
 
-        break;
+            break;
 
         case 'P':
 
-        break;
+            break;
 
         case 'i':
 
-        break;
+            break;
 
         case 'o':
 
-        break;
-      }
-      
-	}
+            break;
+        }
+
+    }
+    Country *ListHead = NULL;
+    /*Criação de um ponteiro que aponta para o tipo de estrutura Country - vai servir para
+    criar novos nós da lista principal que, por sua vez, contém os diferentes países*/
+    Country *newcountry;
+
+    FILE *fp;
+    /*Abertura para leitura do ficheiro criado que irá ter o título indicado*/
+    fp = fopen("covid19_w_tf01.csv","r");
+
+    char line[128];
+
+    /*Usa-se um fgets para ignorar o cabeçalho*/
+    fgets(line,128,fp);
+    /*Este ciclo while que se segue vai percorrer linha a linha todo o ficheiro fornecido pelo professor e irá criar um
+    novo nó da lista principal (a que contém os nomes dos países) sempre que encontarar uma linha com um nome de um
+    país para o qual ainda não existe nó -> deste modo temos assim a nossa Lista principal criada*/
+    while(fgets(line,128,fp) != NULL)
+    {
+        if((newcountry = ReadFile(&ListHead,line)) != NULL)
+        {
+            ListHead = CriaListaPorBaixo(ListHead,newcountry);
+        }
+
+    }
+
+    if(strcmp(L,"all")==0)
+    {
+        printLista(ListHead);
+    }
+    else if (strcmp(L,"continente")==0)
+    {
+        printf("%s", L);
+
+    }
+    else
+    {
+        printf("Erro: comando inserido inválido. \n");
+    }
+    free_list(ListHead);
+
+    fclose(fp);
+
 }
 
 
 
 /*Função para a Linhas de Comandos - ajuda*/
-void CommandLineHelp(){
+void CommandLineHelp()
+{
     printf("-L all -> Lê o ficheiro todo.\n");
     printf("-L continente -> Lê dados relativos ao continente especificado indicado pelo utilizador.\n");
     printf("-S alfa -> Ordem alfabética de países.\n");
@@ -73,3 +117,5 @@ void CommandLineHelp(){
     printf("-o nome_do_ficheiro.csv -> Escrita de dados em ficheiro de texto sendo que o nome_do_ficheiro é introduzido pelo utilizador.\n");
     printf("-o nome_do_ficheiro.data -> Escrita de dados em ficheiro binário sendo que o nome_do_ficheiro é introduzido pelo utilizador.\n");
 }
+
+
